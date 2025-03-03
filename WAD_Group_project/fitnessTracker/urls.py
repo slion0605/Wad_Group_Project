da2c30028c51cf2ee23c15251d12364afef822c2
“Simon <2888818l@student.gla.ac.uk>
2025-03-03 13:53:03 +0000

    making urls

---
+++ WAD_Group_project/fitnessTracker/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rango import views
+
+app_name = 'fitnessTracker'
+
+urlpatterns = [
+path('', views.homepage, name='homepage'),
+path('login/', views.user_login, name='login'),
+path('register/', views.register, name='register'),
+path('catalogue/', views.catalogue, name='catalogue'),
+path('catalogue/<muscle_group>/', views.show_workout, name='show_workout'),
+path('catalogue/<muscle_group>/<workout_name>', views.show_workout, name='show_workout'),
+path('add_workout/', views.add_workout, name='add_workout'),
+path('add_workout/', views.add_meal, name='add_meal'),
+path('tracker/', views.tracker, name='tracker'),
+path('logout/', views.user_logout, name='logout'),
+]
+  
--- WAD_Group_project/fitnessTracker/views.py
+++ WAD_Group_project/fitnessTracker/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def homepage(request):
+    pass
+
+def show_workout(request):
+    pass
+
+def add_workout(request):
+    pass
+
+def add_meal(request):
+    pass
+
+def register(request):
+    pass
+
+def user_login(request):
+    pass
+
+@login_required
+def user_logout(request):
+    pass
+
+def send_home(request):
+    pass
+
+def catalogue(request):
+    pass
+
+def tracker(request):
+    pass
+
+
