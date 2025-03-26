e124e4c18f6ce4c374a03ade14f500a015b358ad
“Wiktor <2763201L@student.gla.ac.uk>
2025-03-26 21:50:21 +0000

    tests

--- WAD_Group_project/fitnessTracker/tests.py
+++ WAD_Group_project/fitnessTracker/tests.py
@@ -25,6 +25,9 @@ class UserProfileModelTest(TestCase):
         self.assertEqual(self.user_profile.goal, 'Lose weight')
         self.assertEqual(self.user_profile.experience, 'Beginner')
 
+    def test_user_profile_str(self):
+        self.assertEqual(str(self.user_profile), 'testuser')
+
 class MealModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass')
@@ -45,6 +48,9 @@ class MealModelTest(TestCase):
         self.assertEqual(self.meal.protein, 30)
         self.assertEqual(self.meal.carbohydrate, 50)
 
+    def test_meal_str(self):
+        self.assertEqual(str(self.meal), 'Test Meal')
+
 class WorkoutModelTest(TestCase):
     def setUp(self):
         self.workout = Workout.objects.create(
@@ -59,7 +65,12 @@ class WorkoutModelTest(TestCase):
         self.assertEqual(self.workout.duration, 60)
         self.assertEqual(self.workout.slug, 'test-workout')
 
+    def test_workout_str(self):
+        self.assertEqual(str(self.workout), 'Test Workout')
 
+    def test_workout_get_embed_url(self):
+        self.workout.video_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        self.assertEqual(self.workout.get_embed_url(), 'https://www.youtube.com/embed/dQw4w9WgXcQ')
 
 class RegistrationFormTest(TestCase):
     def test_registration_form_valid(self):
@@ -101,9 +112,12 @@ class MealFormTest(TestCase):
             'calories': 500,
             'fat': 20,
             'protein': 30,
-            'carbohydrate': 50
+            'carbohydrate': 50,
+            'description': 'This is a test meal.'  # Add description field
         }
         form = MealForm(data=form_data)
+        if not form.is_valid():
+            print(form.errors)  # Print form errors for debugging
         self.assertTrue(form.is_valid())
 
     def test_meal_form_invalid(self):
@@ -112,7 +126,8 @@ class MealFormTest(TestCase):
             'calories': 500,
             'fat': 20,
             'protein': 30,
-            'carbohydrate': 50
+            'carbohydrate': 50,
+            'description': 'This is a test meal.'  # Add description field
         }
         form = MealForm(data=form_data)
         self.assertFalse(form.is_valid())
